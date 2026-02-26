488b2448aa9e2970cbb45c57223cd79c8933470c
Administrator <cds719yyds@outlook.com>
2026-02-26 17:10:50 +0800

    1

---
+++ src/main/java/com/calypso/past_poetrylost_era/Past_PoetryLost_EraDataGenerator.java
@@ -0,0 +1,11 @@
+package com.calypso.past_poetrylost_era;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Past_PoetryLost_EraDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
